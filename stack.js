3617e3b7bf355096373a7cc6f11b37e5130d976e
cagpie <optm.coba@gmail.com>
2016-11-16 21:47:52 +0900

    矢印

--- stack.js
+++ stack.js
@@ -2,34 +2,7 @@
 var stack = document.getElementById('stack');
 var stackWrapper = document.getElementById('stackWrapper');
 var globalMemory = document.getElementById('globalMemory');
-
-//---- int getDisplayY(int id) ----
-// 見える通りの、stackWrapperの上端から、指定したIDの要素の下端との距離を返す
-function getDisplayY(id){
-	return stack.offsetHeight - getY(id) - stackWrapper.scrollTop;
-}
-
-//---- int getY(int id) ----
-// 0番目の要素の下端から、指定したIDの要素の下端との距離を返す
-function getY(id){
-	var sum = 0;
-	for(var i=0; i<id; i++){
-		sum += document.getElementById(i).offsetHeight;
-	}
-	return sum;
-}
-
-//---- void displayMemories(int[] arr) ----
-// 引数に渡した配列の番号(int)のIDのメモリだけ開く
-function displayMemories(arr){
-	[].slice.call(stack.children).forEach(function(memory){
-		if(~arr.indexOf(parseInt(memory.id))){
-			memory.classList.remove('close');
-		} else {
-			memory.classList.add('close');
-		}
-	})
-}
+var stackArrows = [];
 
 //---- Node pushMemory(String str, String textColor, String backgroundColor) ----
 // stackに積む　積んだやつを返す 色の初期値はstack.cssのもの
@@ -62,6 +35,53 @@ function pushGlobalMemory(str, textColor, backgroundColor){
 	});
 	return element;
 }
+
+//---- void displayMemories(int[] arr) ----
+// 引数に渡した配列の番号(int)のIDのメモリだけ開く
+function displayMemories(arr){
+	[].slice.call(stack.children).forEach(function(memory){
+		if(~arr.indexOf(parseInt(memory.id))){
+			memory.classList.remove('close');
+		} else {
+			memory.classList.add('close');
+		}
+	})
+}
+
+//---- Node addStackArrow(int start, int end, String color) ----
+// 始点・終点を指定して矢印を描画
+function addStackArrow(start, end, color){
+	var x = stackWrapper.offsetLeft-10;
+	var y = stackWrapper.offsetTop;
+	var arrow = getArrow(x, y+getDisplayY(start)-20, x, y+getDisplayY(end)-10, 3, color||"#aaa");
+	stackArrows.push(arrow);
+	return arrow;
+}
+
+//---- void clearStackArrow() ----
+//
+function clearStackArrow(){
+	stackArrows.forEach(function(arw){
+		arw.remove();
+	});
+}
+
+//---- int getDisplayY(int id) ----
+// 見える通りの、stackWrapperの上端から、指定したIDの要素の下端との距離を返す
+function getDisplayY(id){
+	return stack.offsetHeight - getY(id) - stackWrapper.scrollTop;
+}
+
+//---- int getY(int id) ----
+// 0番目の要素の下端から、指定したIDの要素の下端との距離を返す
+function getY(id){
+	var sum = 0;
+	for(var i=0; i<id; i++){
+		sum += document.getElementById(i).offsetHeight;
+	}
+	return sum;
+}
+
 
 //------------------------------------------------------------
 //---- Node createElement(String tagName, Object options) ----
